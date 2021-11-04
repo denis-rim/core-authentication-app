459ae758f331b00e7f878b1eb3c26889eceb564a
Denis Mazurchuk <denis.aurorim@gmail.com>
2021-11-04 19:24:35 +0200

    Verifying JSON Web Tokens

--- back-end/src/routes/updateUserInfoRoute.js
+++ back-end/src/routes/updateUserInfoRoute.js
@@ -1,4 +1,5 @@
 import { ObjectID } from "mongodb";
+import { getDbConnection } from "../db";
 
 export const updateUserInfoRoute = {
   path: "/api/users/:userId",
@@ -16,5 +17,40 @@ export const updateUserInfoRoute = {
     }
 
     const token = authorization.split(" ")[1];
+
+    jwt.verify(token, process.env.JWT_SECRET, async (error, decoded) => {
+      if (error)
+        return res.status(401).json({ message: "Unable to verify token" });
+
+      const { id } = decoded;
+
+      if (id !== userId)
+        return res
+          .status(403)
+          .json({ message: "Not allowed to update that user's data" });
+
+      const db = getDbConnection("react-auth");
+      const result = await db
+        .collection("users")
+        .findOneAndUpdate(
+          { _id: ObjectID(id) },
+          { $set: { info: updates } },
+          { returnOriginal: false }
+        );
+      const { email, isVerified, info } = result.value;
+
+      jwt.sign(
+        { id, email, isVerified, info },
+        process.env.JWT_SECRET,
+        { expiresIn: "30d" },
+        (error, token) => {
+          if (error) {
+            return res.status(400).json(error);
+          }
+
+          res.status(200).json({ token });
+        }
+      );
+    });
   },
 };
